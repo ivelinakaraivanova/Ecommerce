854beaaedd7e2939a0aac33b718df949bbcddee6
ivelinakaraivanova <73067985+ivelinakaraivanova@users.noreply.github.com>
2022-10-24 14:23:04 +0300

    Static files settings

--- src/backend/base/models.py
+++ src/backend/base/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from hashlib import blake2b
 from operator import mod
+from pyexpat import model
 from unicodedata import decimal
 from django.db import models
 from django.contrib.auth.models import User
@@ -10,7 +11,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    # image =
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
