3eb5fb290a9fa66a2466de31467fb5b68245561e
ivelinakaraivanova <73067985+ivelinakaraivanova@users.noreply.github.com>
2022-11-08 16:14:33 +0200

    User serializer added

--- src/backend/base/urls.py
+++ src/backend/base/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
+    path('users/profile/', views.getUserProfile, name='user-profile'),
     path('products/<str:pk>/', views.getProduct, name='product'),
 ]
